51db78a568ef11a9923251ecc9ffdb1564657378
Victor.Manzini <victor@robertomanzini.com.br>
2026-03-24 17:17:56 -0300

    Finalizado os exercicios pós aula 3

---
+++ semestre1/exercicios/aula3/exercicios_pos_aula3.3.py
@@ -0,0 +1,9 @@
+import numpy as np 
+
+x = np.linspace(0, 15, 50)
+
+a = 2
+b = 3
+
+y = a * x + b 
+print(y)
--- semestre1/exercicios/exercicios_pos_aula3.py
+++
@@ -1,5 +0,0 @@
-import numpy as np 
-
-a = np.array([7, 1 , 0, -5, 4, -12, 2, -1, 6, -7])
-print(np.max(a))
-print(np.min(a))
